b38e9c7a41520a211a6d5c8c52adb01e237454c6
Edan Odongo <edanodongo05@gmail.com>
2025-07-20 14:28:40 +0300

    updated views.py

--- product-service/catalog/views.py
+++ product-service/catalog/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Product
+from .serializers import ProductSerializer
 
-# Create your views here.
+# This view handles listing all products and creating a new product.
+class ProductListCreateView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
