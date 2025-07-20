86cf22d8c721798e9e73f7e981552ff492b19725
Edan Odongo <edanodongo05@gmail.com>
2025-07-20 10:49:49 +0300

    updated settings.py

--- user-service/userservice/settings.py
+++ user-service/userservice/settings.py
@@ -14,8 +14,8 @@ from pathlib import Path
 from decouple import config, Csv
 
 SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=False, cast=bool)
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*', cast=Csv)
+DEBUG = config('DEBUG')
+ALLOWED_HOSTS = ['*']  # Adjust as needed for production
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
