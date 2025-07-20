69507542a8c424f06139e47d2fa705615cf2bb78
Edan Odongo <edanodongo05@gmail.com>
2025-07-20 14:27:56 +0300

    added serializer.py

--- product-service/catalog/models.py
+++ product-service/catalog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+# This file defines the Product model which represents the product data in the database.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    inventory = models.IntegerField()
+
+    def __str__(self):
+        return self.name
---
+++ product-service/catalog/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from .models import Product
+
+# Serializers define the API representation.
+# This serializer will convert the Product model instances into JSON format
+# and validate incoming data for creating or updating Product instances.
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
