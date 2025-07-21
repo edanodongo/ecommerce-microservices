89be6a6b4935e8f42607bbfb3b54108197db58d6
Edan Odongo <edanodongo05@gmail.com>
2025-07-21 09:34:11 +0300

    updated OrderListCreateView

--- order-service/orders/views.py
+++ order-service/orders/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Order
+from .serializers import OrderSerializer
 
-# Create your views here.
+# Order List and Create View
+# This view allows listing all orders and creating a new order.
+class OrderListCreateView(generics.ListCreateAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
