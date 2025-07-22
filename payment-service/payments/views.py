7faa7faed850ad38cd58d151c74a1e1426001b11
Edan Odongo <edanodongo05@gmail.com>
2025-07-22 20:00:48 +0300

    updated payment-service/payments

--- payment-service/payments/views.py
+++ payment-service/payments/views.py
@@ -1,9 +1,21 @@
-from rest_framework import generics
+# from rest_framework import generics
+# from .models import Payment
+# from .serializers import PaymentSerializer
+
+# # Payment List and Create View
+# # This view allows listing all payments and creating a new payment.
+# class PaymentListCreateView(generics.ListCreateAPIView):
+#     queryset = Payment.objects.all()
+#     serializer_class = PaymentSerializer
+
+
+from rest_framework import viewsets
 from .models import Payment
 from .serializers import PaymentSerializer
 
-# Payment List and Create View
-# This view allows listing all payments and creating a new payment.
-class PaymentListCreateView(generics.ListCreateAPIView):
+# Payment ViewSet
+# This view handles listing all payments and creating a new payment.
+class PaymentViewSet(viewsets.ModelViewSet):
     queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
+    
