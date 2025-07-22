641261f3dc77688d4ce006d9349b51bf159ccd3b
Edan Odongo <edanodongo05@gmail.com>
2025-07-22 19:48:39 +0300

    updated user-service/accounts

--- user-service/accounts/models.py
+++ user-service/accounts/models.py
@@ -1,8 +1,16 @@
+# from django.contrib.auth.models import AbstractUser
+# from django.db import models
+
+# class CustomUser(AbstractUser):
+#     email = models.EmailField(unique=True)
+
+#     def __str__(self):
+#         return self.username
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+# CustomUser model extending AbstractUser
+# with additional fields if needed
 class CustomUser(AbstractUser):
-    email = models.EmailField(unique=True)
-
-    def __str__(self):
-        return self.username
+    pass  # extend with more fields if needed
--- user-service/accounts/serializers.py
+++ user-service/accounts/serializers.py
@@ -1,8 +1,25 @@
+# from rest_framework import serializers
+# from .models import CustomUser
+
+# # Serializers for the CustomUser model
+# class UserSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = CustomUser
+#         fields = ['id', 'username', 'email']
+
+
 from rest_framework import serializers
 from .models import CustomUser
+from django.contrib.auth.password_validation import validate_password
 
 # Serializers for the CustomUser model
-class UserSerializer(serializers.ModelSerializer):
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, validators=[validate_password])
+
     class Meta:
         model = CustomUser
-        fields = ['id', 'username', 'email']
+        fields = ('username', 'email', 'password')
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(**validated_data)
+        return user
