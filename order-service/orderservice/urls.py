4af47a9c4345f946738e8cf55037f5f9ae815a18
Edan Odongo <edanodongo05@gmail.com>
2025-07-22 19:58:35 +0300

    updated order-service/main app urls

--- order-service/orderservice/urls.py
+++ order-service/orderservice/urls.py
@@ -1,8 +1,22 @@
-from django.contrib import admin
-from django.urls import path
-from orders.views import OrderListCreateView
+# from django.contrib import admin
+# from django.urls import path
+# from orders.views import OrderListCreateView
 
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('api/orders/', OrderListCreateView.as_view(), name='order-list-create'),
+# ]
+
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from orders.views import OrderViewSet
+
+# Create a router and register the OrderViewSet with it
+router = DefaultRouter()
+router.register(r'orders', OrderViewSet)
+
+# Include the router URLs in the urlpatterns
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/orders/', OrderListCreateView.as_view(), name='order-list-create'),
+    path('orders/', include(router.urls)),
 ]
