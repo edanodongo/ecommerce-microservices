d3366e41e1552e713ece1f335e3ebf842324d17f
Edan Odongo <edanodongo05@gmail.com>
2025-07-22 20:01:48 +0300

    updated payment-service/main app urls

--- payment-service/paymentservice/urls.py
+++ payment-service/paymentservice/urls.py
@@ -1,8 +1,23 @@
-from django.contrib import admin
-from django.urls import path
-from payments.views import PaymentListCreateView
+# from django.contrib import admin
+# from django.urls import path
+# from payments.views import PaymentListCreateView
 
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('api/payments/', PaymentListCreateView.as_view(), name='payment-list-create'),
+# ]
+
+
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from payments.views import PaymentViewSet
+
+# Create a router and register the PaymentViewSet with it
+router = DefaultRouter()
+router.register(r'payments', PaymentViewSet)
+
+# Include the router URLs in the urlpatterns
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/payments/', PaymentListCreateView.as_view(), name='payment-list-create'),
+    path('payments/', include(router.urls)),
 ]
