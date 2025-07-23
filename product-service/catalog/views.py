d5824b2b8a0eb3acad389f096dc74ff758d892eb
Edan Odongo <edanodongo05@gmail.com>
2025-07-23 20:03:40 +0300

    updated product-service

--- product-service/catalog/views.py
+++ product-service/catalog/views.py
@@ -1,14 +1,3 @@
-# from rest_framework import generics
-# from .models import Product
-# from .serializers import ProductSerializer
-
-# # This view handles listing all products and creating a new product.
-# class ProductListCreateView(generics.ListCreateAPIView):
-#     queryset = Product.objects.all()
-#     serializer_class = ProductSerializer
-
-
-
 from rest_framework import viewsets
 from .models import Product
 from .serializers import ProductSerializer
