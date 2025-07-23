da777c7dc95e32c42cb57c76092fb55e1ac8abb0
Edan Odongo <edanodongo05@gmail.com>
2025-07-23 20:00:22 +0300

    updated payment-service

--- payment-service/payments/serializers.py
+++ payment-service/payments/serializers.py
@@ -1,13 +1,3 @@
-# from rest_framework import serializers
-# from .models import Payment
-
-# # Payment Serializer
-# # This serializer is used to convert Payment model instances into JSON format and vice versa.
-# class PaymentSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Payment
-#         fields = '__all__'
-
 
 from rest_framework import serializers
 from .models import Payment
--- payment-service/payments/views.py
+++ payment-service/payments/views.py
@@ -1,14 +1,4 @@
-# from rest_framework import generics
-# from .models import Payment
-# from .serializers import PaymentSerializer
-
-# # Payment List and Create View
-# # This view allows listing all payments and creating a new payment.
-# class PaymentListCreateView(generics.ListCreateAPIView):
-#     queryset = Payment.objects.all()
-#     serializer_class = PaymentSerializer
-
-
+# payments/views.py
 from rest_framework import viewsets
 from .models import Payment
 from .serializers import PaymentSerializer
