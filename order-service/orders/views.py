09770e87ef366e8526e724e18543f82e2d4d50f3
Edan Odongo <edanodongo05@gmail.com>
2025-07-23 15:09:29 +0300

    update to order-service/order views.py

--- order-service/orders/views.py
+++ order-service/orders/views.py
@@ -1,12 +1,12 @@
-from rest_framework import generics
-from .models import Order
-from .serializers import OrderSerializer
+# from rest_framework import generics
+# from .models import Order
+# from .serializers import OrderSerializer
 
-# Order List and Create View
-# This view allows listing all orders and creating a new order.
-class OrderListCreateView(generics.ListCreateAPIView):
-    queryset = Order.objects.all()
-    serializer_class = OrderSerializer
+# # Order List and Create View
+# # This view allows listing all orders and creating a new order.
+# class OrderListCreateView(generics.ListCreateAPIView):
+#     queryset = Order.objects.all()
+#     serializer_class = OrderSerializer
 
 
 
