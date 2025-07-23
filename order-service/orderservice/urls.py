25fcaef1e79aaa6ebdc4082557a35ec32d4aedbb
Edan Odongo <edanodongo05@gmail.com>
2025-07-23 19:52:30 +0300

    updated order-service

--- order-service/orderservice/urls.py
+++ order-service/orderservice/urls.py
@@ -1,12 +1,3 @@
-# from django.contrib import admin
-# from django.urls import path
-# from orders.views import OrderListCreateView
-
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     path('api/orders/', OrderListCreateView.as_view(), name='order-list-create'),
-# ]
-
 
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
